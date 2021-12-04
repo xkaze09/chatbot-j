9e1d531b2ffe9751f82e3510d496b33e157a5f2f
CJ <47445339+xkaze09@users.noreply.github.com>
2021-12-04 23:11:09 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask
+from flask import request, jsonify
+import os
+import dialogflow
+from google.api_core.exceptions import InvalidArgument
+import requests
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'private_key.json'
+
+DIALOGFLOW_PROJECT_ID = 'chatbot-j-xqv9'
+DIALOGFLOW_LANGUAGE_CODE = 'en'
+SESSION_ID = 'me'
+
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route('/')
+def root():
+    return "Hello World"
+
+@app.route('/api/getMessage', methods=['POST'])
+def home():
+    return ""
+
+
+#def sendMessage(mobnum, message):
+
+
+if __name__ == '__main__':
+    app.run()
