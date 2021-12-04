bf23fcca717526bdf2cba46de508b82a4b192295
CJ <47445339+xkaze09@users.noreply.github.com>
2021-12-04 23:20:56 +0800

    Add main script

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import os
 from google.cloud import dialogflow
 from google.api_core.exceptions import InvalidArgument
 import requests
+from werkzeug.datastructures import LanguageAccept
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = 'private_key.json'
 
@@ -14,18 +15,30 @@ SESSION_ID = 'me'
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
+
 @app.route('/')
 def root():
     return "Hello World"
 
+
 @app.route('/api/getMessage', methods=['POST'])
 def home():
     # Main Code
-    
-    return ""
+    message = request.form.get('Body')
+    mobnum = request.form.get('From')
+    session_client = dialogflow.SessionsClient()
+    session = session_client.session_path(DIALOGFLOW_PROJECT_ID, SESSION_ID)
+    text_input = dialogflow.types.TextInput(
+        text=message, language_code=DIALOGFLOW_LANGUAGE_CODE)
+    query_input = dialogflow.types.QueryInput(text=text_input)
+    try:
+        response = session_client.detect_intent(
+            session=session, query_input=query_input)
+    except InvalidArgument:
+        raise
 
 
-#def sendMessage(mobnum, message):
+# def sendMessage(mobnum, message):
 
 
 if __name__ == '__main__':
